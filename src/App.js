462597dd1fa1cb5c8230e3fb3d668873aae51139
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2020-06-04 07:28:23 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     return ( <
         div className = "App" >
         <
-        h1 > Hello World < /h1>  < /
+        h1 > Hello World from UHK < /h1>  < /
         div >
     );
 }
